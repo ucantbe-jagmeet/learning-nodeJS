06381c7d25012f58e099b1e88365c77009b732d8
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-01-19 09:52:31 +0530

    about package.json

---
+++ node-code-step-by-step/6-About-packageJSON.js
@@ -0,0 +1,9 @@
+/*
+    It store details of the project like version of the techniques , git name , project name , command names , package names, 
+     
+    Q? how to create package.json
+    A. npm init which means intialise 
+       npm working on managing the package of the node like command , version etc.
+       
+
+*/
