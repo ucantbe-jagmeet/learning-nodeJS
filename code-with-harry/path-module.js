aef96b73c4c1972a7ad3ab010c07a61fa1cc849a
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-03-02 20:20:10 +0530

    nodejs

--- code-with-harry/path-module.js
+++ code-with-harry/path-module.js
@@ -3,6 +3,6 @@ const a = path.basename(" C: \\temp\\myfile.html");
 const b = path.dirname(" C: \\temp\\myfile.html");
 console.log(a);
 console.log(b);
-const c = path.extname(__filename);
-console.log(__filename, c);
+const c1 = path.extname(__filename);
+console.log(__filename, c1);
 console.log(__dirname);
