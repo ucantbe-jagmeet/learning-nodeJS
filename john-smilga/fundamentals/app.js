c7cc395111ce169da5481530be0c01ea16b4e09c
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-08 01:32:21 +0530

    HTTP module more features

--- john-smilga/fundamentals/app.js
+++ john-smilga/fundamentals/app.js
@@ -2,8 +2,19 @@
 const http = require("http");
 
 const server = http.createServer((request, response) => {
-  response.write("Welcome to our Home page");
-  response.end();
+  if (request.url === "/") {
+    response.end("Welcome to our home page");
+    return;
+  }
+
+  if (request.url === "/about") {
+    response.end("Welcome to our about page");
+    return;
+  }
+
+  response.end(`<h1>Oops...</h1>
+  <p> We can't seem to find the page you are looking for</p>
+  <a href="/">back home</a>`);
 });
 
 server.listen(5000);
