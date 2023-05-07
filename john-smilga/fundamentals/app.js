f7cacb41b4e32b3c8b999c3b8f2f6180b2a36ba3
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-07 20:32:23 +0530

    First Module

--- john-smilga/fundamentals/app.js
+++ john-smilga/fundamentals/app.js
@@ -1,11 +1,9 @@
-// Modules
-
-const john = "john";
-const peter = "peter";
-
-const sayHi = (name) => {
-  console.log(`hello there ${name}`);
-};
+// CommonJs, every file is module (by default)
+// Modules - Encapsulated Code ( only share minimum)
+const names = require("./4-names");
+const sayHi = require("./5-utils");
+console.log(names);
 
 sayHi("susan");
-sayHi(john);
+sayHi(names.john);
+sayHi(names.peter);
