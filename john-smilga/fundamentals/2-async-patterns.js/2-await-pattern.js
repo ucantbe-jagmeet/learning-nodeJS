a5c579809a50667213af01ac5b2997c0975cc679
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-08 04:01:43 +0530

    Await Patterns

--- john-smilga/fundamentals/2-async-patterns.js/2-await-pattern.js
+++ john-smilga/fundamentals/2-async-patterns.js/2-await-pattern.js
@@ -1,15 +1,15 @@
-const { readFile, writeFile } = require("fs").promises;
-// const util = require("util");
-// const readFilePromise = util.promisify(readFile);
-// const writeFilePromise = util.promisify(writeFile);
+const { readFile, writeFile } = require("fs");
+const util = require("util");
+const readFilePromise = util.promisify(readFile);
+const writeFilePromise = util.promisify(writeFile);
 
 const start = async () => {
   try {
-    const first = await readFile("./content/first.txt", "utf8");
-    const second = await readFile("./content/second.txt", "utf8");
+    const first = await readFilePromise("./content/first.txt", "utf8");
+    const second = await readFilePromise("./content/second.txt", "utf8");
     writeFilePromise(
       "./content/result-sync.txt",
-      `WOW this promise is awesome ${first} , ${second}`
+      `WOW this util is awesome ${first} , ${second}`
     );
     console.log(first, second);
   } catch (error) {
