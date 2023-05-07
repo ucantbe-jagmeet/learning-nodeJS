2335d609a1b22db140ba45d792487e9e2a83db89
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-07 23:12:07 +0530

    OS Module

---
+++ john-smilga/fundamentals/3-modules.js
@@ -0,0 +1,11 @@
+// CommonJs, every file is module (by default)
+// Modules - Encapsulated Code ( only share minimum)
+const names = require("./4-names");
+const sayHi = require("./5-utils");
+const data = require("./6-alternative-approach,js");
+
+require("./7-mind-grenade");
+
+// sayHi("susan");
+// sayHi(names.john);
+// sayHi(names.peter);
