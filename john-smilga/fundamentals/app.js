dfc9cab21fecf4f8b983c91349efb2bc199319e4
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-07 20:47:45 +0530

    Mind Grenade

--- john-smilga/fundamentals/app.js
+++ john-smilga/fundamentals/app.js
@@ -2,8 +2,10 @@
 // Modules - Encapsulated Code ( only share minimum)
 const names = require("./4-names");
 const sayHi = require("./5-utils");
-console.log(names);
+const data = require("./6-alternative-approach,js");
 
-sayHi("susan");
-sayHi(names.john);
-sayHi(names.peter);
+require("./7-mind-grenade");
+
+// sayHi("susan");
+// sayHi(names.john);
+// sayHi(names.peter);
