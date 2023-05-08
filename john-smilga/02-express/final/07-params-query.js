91b2287d669b2f9b4cf793f6e57c25faaade0816
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-09 01:58:40 +0530

    Params Query Additional 2

--- john-smilga/02-express/final/07-params-query.js
+++ john-smilga/02-express/final/07-params-query.js
@@ -3,7 +3,7 @@
 
 const express = require("express");
 const app = express();
-const { products } = require("../data");
+const { products } = require("./data");
 
 app.get("/", (req, res) => {
   res.send(`<h1>Home Page</h1> <a href="/api/products">Products</a>`);
@@ -30,6 +30,27 @@ app.get("/api/products/:productID", (req, res) => {
   }
 
   return res.json(singleProduct);
+});
+
+app.get("/api/v1/query", (req, res) => {
+  // console.log(req.query);
+  const { search, limit } = req.query;
+  let sortedProducts = [...products];
+
+  if (search) {
+    sortedProducts = sortedProducts.filter((product) => {
+      return product.name.startsWith(search);
+    });
+  }
+  if (limit) {
+    sortedProducts = sortedProducts.slice(0, Number(limit));
+  }
+
+  if (sortedProducts.length < 1) {
+    // res.status(200).send(`No Products match your search`);
+    return res.status(200).json({ Success: true, data: [] });
+  }
+  res.status(200).json(sortedProducts);
 });
 
 app.listen(5000, () => {
