c190add6f2c2578fd018a6e876834435e5b5477b
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-10 02:43:31 +0530

    getAllTask controller

--- john-smilga/03-task-manager/controllers/tasks.js
+++ john-smilga/03-task-manager/controllers/tasks.js
@@ -1,7 +1,12 @@
 const Task = require("../models/task");
 
-const getAllTasks = (req, res) => {
-  res.send("all items from the File");
+const getAllTasks = async (req, res) => {
+  try {
+    const task = await Task.find({});
+    res.status(200).json({ tasks: task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 const createTask = async (req, res) => {
