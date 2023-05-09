1bf64051fff65ff1108f48ff63161bd8c03e1097
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-10 00:33:36 +0530

    server connected to mango db

--- john-smilga/03-task-manager/db/connect.js
+++ john-smilga/03-task-manager/db/connect.js
@@ -1,4 +1,14 @@
 const mongoose = require("mongoose");
 
 const connectionString =
-  "mongodb+srv://ucantbe-jagmeet:<password>@cluster0.wzawilj.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://ucantbe-jagmeet:Jagmeet787600@cluster0.wzawilj.mongodb.net/03-TASK-MANAGER?retryWrites=true&w=majority";
+
+mongoose
+  .connect(connectionString, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => console.log(`connected to the DB`))
+  .catch((err) => console.log(err));
