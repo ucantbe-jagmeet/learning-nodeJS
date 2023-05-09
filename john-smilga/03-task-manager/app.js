7ec61868b4c29a0680f131c45efeeb7c9b8f92a0
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-10 05:03:54 +0530

    PORT env

--- john-smilga/03-task-manager/app.js
+++ john-smilga/03-task-manager/app.js
@@ -18,14 +18,12 @@ app.use("/api/v1/tasks", tasks);
 app.use(notFound);
 app.use(errorHandlerMiddleware);
 
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 const start = async () => {
   try {
     await connectDB(process.env.MONGO_URI);
-    app.listen(port, () => {
-      console.log(`Server is listening on the port ${port}...`);
-    });
+    app.listen(port, console.log(`Server is listening on the port ${port}...`));
   } catch (error) {
     console.log(error);
   }
