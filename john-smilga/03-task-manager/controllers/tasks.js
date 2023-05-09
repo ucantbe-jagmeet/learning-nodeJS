b26ad48aceda7407ec7053977c81f471b0ae0c6b
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-10 03:13:06 +0530

    Update Task

--- john-smilga/03-task-manager/controllers/tasks.js
+++ john-smilga/03-task-manager/controllers/tasks.js
@@ -23,7 +23,7 @@ const getTask = async (req, res) => {
     const { id: taskID } = req.params;
     const task = await Task.findOne({ _id: taskID });
 
-    if (!taskID) {
+    if (!task) {
       return res.status(404).json({ msg: `No task with id: ${taskID}` });
     }
 
@@ -37,8 +37,20 @@ const updateTask = (req, res) => {
   res.send("Update Task");
 };
 
-const deleteTask = (req, res) => {
-  res.send("Delete Task");
+const deleteTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndDelete({ _id: taskID });
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+
+    res.status(200).json({ task });
+    // res.status(200).send()
+    // res.status(200).json({ task: null, status: `success` });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 
 module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
