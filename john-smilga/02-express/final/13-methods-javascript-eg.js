1acfee763606b8042be758fe30284ae16375c42d
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-09 05:30:18 +0530

    Methods Put postman

--- john-smilga/02-express/final/13-methods-javascript-eg.js
+++ john-smilga/02-express/final/13-methods-javascript-eg.js
@@ -1,7 +1,7 @@
 // METHODS
 const express = require("express");
 const app = express();
-let { people } = require("./data");
+let { people } = require("../data");
 
 // get -> read the data
 // post -> Insert the data
@@ -37,27 +37,6 @@ app.post("/api/postman/people", (req, res) => {
       .json({ success: false, msg: "Please provide name value" });
   }
   res.status(201).json({ success: true, data: [...people, name] });
-});
-
-app.put("/api/people/:id", (req, res) => {
-  const { id } = req.params;
-  const { name } = req.body;
-
-  const person = people.find((person) => person.id === Number(id));
-  if (!person) {
-    return res
-      .status(404)
-      .json({ success: false, msg: `No Person with id : ${id}` });
-  }
-
-  const newPeople = people.map((person) => {
-    if (person.id === Number(id)) {
-      person.name = name;
-    }
-    return person;
-  });
-
-  res.status(200).json({ success: true, data: newPeople });
 });
 
 app.post("/login", (req, res) => {
