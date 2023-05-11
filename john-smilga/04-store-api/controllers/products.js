cf012bca086995d1fe1dcc84a7864278133da828
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-12 04:22:12 +0530

    Numeric Filters

--- john-smilga/04-store-api/controllers/products.js
+++ john-smilga/04-store-api/controllers/products.js
@@ -10,7 +10,7 @@ const getAllProductsStatic = async (req, res) => {
   res.status(200).json({ msg: `Products Testing Route` });
 };
 const getAllProducts = async (req, res) => {
-  const { featured, company, name, sort, fields } = req.query;
+  const { featured, company, name, sort, fields, numericFilters } = req.query;
   const queryObject = {};
 
   if (featured) {
@@ -22,6 +22,26 @@ const getAllProducts = async (req, res) => {
   if (name) {
     queryObject.name = { $regex: name, $options: "i" };
   }
+
+  if (numericFilters) {
+    const operatorMap = {
+      ">": "$gt",
+      "=": "$gte",
+      "=": "$eq",
+      "<": "$lt",
+      "<=": "$lte",
+    };
+
+    const regEx = /\b( <|>|>=|=|<|<= )\b/g;
+
+    let filters = numericFilters.replace(
+      regEx,
+      (match) => `-${operatorMap[match]}-`
+    );
+
+    console.log(filters);
+  }
+
   //   console.log(queryObject);
 
   let result = Product.find(queryObject);
