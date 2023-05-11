fc77ff445ff4888fc5a24c096e7ee4bbbfc32487
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-11 23:13:57 +0530

    connect to dv

--- john-smilga/04-store-api/app.js
+++ john-smilga/04-store-api/app.js
@@ -4,6 +4,8 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 
+const connectDB = require("./db/connect");
+
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
 
@@ -26,6 +28,8 @@ const port = process.env.PORT || 3000;
 const start = async () => {
   try {
     // connect to db
+    await connectDB(process.env.MONGO_URI);
+
     app.listen(port, console.log(`Server is listening to the port ${port}`));
   } catch (error) {
     console.log(error);
