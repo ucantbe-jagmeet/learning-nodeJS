9af3cdd7271ea8b3ee8d21a0e11e67770275d33a
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-12 04:05:00 +0530

    Pagination

--- john-smilga/04-store-api/controllers/products.js
+++ john-smilga/04-store-api/controllers/products.js
@@ -3,7 +3,8 @@ const Product = require("../models/product");
 const getAllProductsStatic = async (req, res) => {
   //   throw new Error("testing async errors");
 
-  const products = await Product.find({}).select("name price");
+  const products = await Product.find({}).sort("name").select("name price");
+
   res.status(200).json({ products, nbHits: products.length });
 
   res.status(200).json({ msg: `Products Testing Route` });
@@ -36,6 +37,12 @@ const getAllProducts = async (req, res) => {
     const fieldsList = fields.split(",").join(" ");
     result = result.select(fieldsList);
   }
+
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+
+  const skip = (page - 1) * limit;
+  result = result.skip(skip).limit(limit);
 
   const products = await result;
 
