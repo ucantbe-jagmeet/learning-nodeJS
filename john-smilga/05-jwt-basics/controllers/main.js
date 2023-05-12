fe4ceabb5f35093ecfa02af8ebda6064598b00ad
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-13 03:26:46 +0530

    Sign JWT

--- john-smilga/05-jwt-basics/controllers/main.js
+++ john-smilga/05-jwt-basics/controllers/main.js
@@ -1,3 +1,4 @@
+const jwt = require("jsonwebtoken");
 const CustomAPIError = require("../errors/custom-error");
 
 const login = async (req, res) => {
@@ -9,8 +10,15 @@ const login = async (req, res) => {
   if (!username || !password) {
     throw new CustomAPIError("Please provide email and password", 400);
   }
-  console.log(username, password);
-  res.send("Fake Login/Register/Signup route");
+
+  // try to keep payload small, better experience for user
+  const id = new Date().getDate();
+
+  const token = jwt.sign({ id, username }, process.env.JWT_SECRET, {
+    expiresIn: "30d",
+  });
+
+  res.status(200).send({ msg: "User Created", token });
 };
 
 const dashboard = async (req, res) => {
