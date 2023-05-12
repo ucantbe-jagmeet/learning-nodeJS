38deae7f298241ac1ba216d91779d36e1825d379
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-13 03:50:11 +0530

    Check Auth Header

--- john-smilga/05-jwt-basics/controllers/main.js
+++ john-smilga/05-jwt-basics/controllers/main.js
@@ -22,11 +22,20 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  console.log(req.headers);
+  const authHeader = req.headers.authorization;
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  console.log(token);
+
   const luckyNumber = Math.floor(Math.random() * 99);
   res.status(200).json({
     msg: `Hello,  User`,
-    secret: `Here is your authorized data, your lucky number is ${luckyNumber} `,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
   });
 };
 
