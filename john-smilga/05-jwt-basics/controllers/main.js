05cef60bb5f39a90cb7c16f1587061dc38d857df
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-13 04:15:31 +0530

    Auth middleware completed

--- john-smilga/05-jwt-basics/controllers/main.js
+++ john-smilga/05-jwt-basics/controllers/main.js
@@ -22,24 +22,12 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  const authHeader = req.headers.authorization;
-
-  if (!authHeader || !authHeader.startsWith("Bearer ")) {
-    throw new CustomAPIError("No token provided", 401);
-  }
-
-  const token = authHeader.split(" ")[1];
-
-  try {
-    const decoded = jwt.verify(token, process.env.JWT_SECRET);
-    const luckyNumber = Math.floor(Math.random() * 99);
-    res.status(200).json({
-      msg: `Hello, ${decoded.username}`,
-      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-    });
-  } catch (error) {
-    throw new CustomAPIError("Not Authorized to access this route", 401);
-  }
+  console.log(req.user);
+  const luckyNumber = Math.floor(Math.random() * 99);
+  res.status(200).json({
+    msg: `Hello, ${req.user.username}`,
+    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+  });
 };
 
 module.exports = { login, dashboard };
