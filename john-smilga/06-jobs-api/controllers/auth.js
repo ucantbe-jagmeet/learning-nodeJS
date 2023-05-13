8e99d1117dda2fe24069cb76b3f500f3650ea3e8
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-14 01:29:34 +0530

    auth middleware setup

--- john-smilga/06-jobs-api/controllers/auth.js
+++ john-smilga/06-jobs-api/controllers/auth.js
@@ -16,10 +16,17 @@ const login = async (req, res) => {
   }
 
   const user = await User.findOne({ email });
-  // code for comparing pasd
   if (!user) {
     throw new UnauthenticatedError("Invalid Credentials");
   }
+
+  const isPasswordCorrect = await user.comparePassword(password);
+
+  if (!isPasswordCorrect) {
+    throw new UnauthenticatedError("Invalid Credentials");
+  }
+
+  // compare password
 
   const token = user.createJWT();
   res.status(StatusCodes.OK).json({ user: { name: user.name }, token });
