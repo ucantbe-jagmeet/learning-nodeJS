6f53a5357c7883080243abba52780dd3174ada57
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-14 15:20:17 +0530

    Validation Error

--- john-smilga/06-jobs-api/middleware/error-handler.js
+++ john-smilga/06-jobs-api/middleware/error-handler.js
@@ -7,9 +7,17 @@ const errorHandlerMiddleware = (err, req, res, next) => {
     msg: err.message || "Something went wrong try again later",
   };
 
-  if (err instanceof CustomAPIError) {
-    return res.status(err.statusCode).json({ msg: err.message });
+  // if (err instanceof CustomAPIError) {
+  //   return res.status(err.statusCode).json({ msg: err.message });
+  // }
+
+  if (err.name === "ValidationError") {
+    customError.msg = Object.values(err.errors)
+      .map((item) => item.message)
+      .join(",");
+    customError.statusCode = 400;
   }
+
   if (err.code && err.code === 11000) {
     customError.msg = `Duplicate value entered for ${Object.keys(
       err.keyValue
