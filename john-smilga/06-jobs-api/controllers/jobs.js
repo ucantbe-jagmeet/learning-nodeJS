99e265906cf03ded6ab91c284f5eb5cd07102090
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-14 14:16:39 +0530

    Update Job

--- john-smilga/06-jobs-api/controllers/jobs.js
+++ john-smilga/06-jobs-api/controllers/jobs.js
@@ -7,6 +7,7 @@ const getAllJobs = async (req, res) => {
 
   res.status(StatusCodes.OK).send({ jobs, count: jobs.length });
 };
+
 const getJob = async (req, res) => {
   const {
     user: { userId },
@@ -23,15 +24,37 @@ const getJob = async (req, res) => {
 
   res.status(StatusCodes.OK).json({ job });
 };
+
 const createJob = async (req, res) => {
   req.body.createdBy = req.user.userId;
   const job = await Job.create(req.body);
 
   res.status(StatusCodes.CREATED).json({ job });
 };
+
 const updateJob = async (req, res) => {
-  res.send("update job");
+  const {
+    body: { company, position },
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  if (company === "" || position === "") {
+    throw new BadRequestError("Company or Position Fields cannot be empty");
+  }
+
+  const job = await Job.findByIdAndUpdate(
+    { _id: jobId, createdBy: userId },
+    req.body,
+    { new: true, runValidators: true }
+  );
+
+  if (!job) {
+    throw new NotFoundError(`No Job with id ${jobId}`);
+  }
+  res.status(StatusCodes.OK).json({ job });
 };
+
 const deleteJob = async (req, res) => {
   res.send("delete job");
 };
