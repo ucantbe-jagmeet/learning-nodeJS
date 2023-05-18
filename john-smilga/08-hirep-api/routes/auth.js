fb248bd453ae7aebf5371d58638f08497fb82836
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-18 22:42:03 +0530

    update user - completed

--- john-smilga/08-hirep-api/routes/auth.js
+++ john-smilga/08-hirep-api/routes/auth.js
@@ -1,7 +1,11 @@
-const express = require('express')
-const router = express.Router()
-const { register, login } = require('../controllers/auth')
-router.post('/register', register)
-router.post('/login', login)
+const express = require("express");
+const router = express.Router();
 
-module.exports = router
+const authenticateUser = require("../middleware/authentication");
+
+const { register, login, updateUser } = require("../controllers/auth");
+router.post("/register", register);
+router.post("/login", login);
+router.patch("/updateUser", authenticateUser, updateUser);
+
+module.exports = router;
