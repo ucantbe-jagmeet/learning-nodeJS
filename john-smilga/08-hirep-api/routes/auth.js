3194f767f39cc2ca4f5f2ffe7dae18b5e7b09c2a
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-19 02:29:14 +0530

    Api limiter

--- john-smilga/08-hirep-api/routes/auth.js
+++ john-smilga/08-hirep-api/routes/auth.js
@@ -4,8 +4,18 @@ const authenticateUser = require("../middleware/authentication");
 const testUser = require("../middleware/testUser");
 const { register, login, updateUser } = require("../controllers/auth");
 
-router.post("/register", register);
-router.post("/login", login);
+const rateLimiter = require("express-rate-limit");
+
+const apiLimiter = rateLimiter({
+  windowsMs: 15 * 60 * 100,
+  max: 10,
+  message: {
+    msg: "too many requests from this IP, Please try again after 15 minutes",
+  },
+});
+
+router.post("/register", apiLimiter, register);
+router.post("/login", apiLimiter, login);
 router.patch("/updateUser", authenticateUser, testUser, updateUser);
 
 module.exports = router;
