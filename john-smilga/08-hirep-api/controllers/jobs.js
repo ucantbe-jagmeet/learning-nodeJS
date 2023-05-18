59220f4ee3211416e6a21a82836503a7218a61cc
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-19 02:59:33 +0530

    Refactor stats data

--- john-smilga/08-hirep-api/controllers/jobs.js
+++ john-smilga/08-hirep-api/controllers/jobs.js
@@ -2,6 +2,9 @@ const Job = require("../models/Job");
 const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 
+const mongoose = require("mongoose");
+const moment = require("moment");
+
 const getAllJobs = async (req, res) => {
   const { search, status, jobType, sort } = req.query;
 
@@ -111,9 +114,28 @@ const deleteJob = async (req, res) => {
 };
 
 const showStats = async (req, res) => {
+  let stats = await Job.aggregate([
+    { $match: { createdBy: mongoose.Types.ObjectId(req.user.userId) } },
+    { $group: { _id: "$status", count: { $sum: 1 } } },
+  ]);
+
+  stats = stats.reduce((acc, curr) => {
+    const { _id: title, count } = curr;
+    acc[title] = count;
+    return acc;
+  }, {});
+
+  // console.log(stats);
+
+  const defaultStats = {
+    pending: stats.pending || 0,
+    interview: stats.interview || 0,
+    declined: stats.declined || 0,
+  };
+
   res
     .status(StatusCodes.OK)
-    .json({ defaultStats: {}, monthlyApplications: [] });
+    .json({ defaultStats: defaultStats, monthlyApplications: [] });
 };
 
 module.exports = {
