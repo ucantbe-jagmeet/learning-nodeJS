5f6228735d9abfacbc0c36bd0bdb2441ac9c77b4
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-19 20:21:20 +0530

    Cloudinary Setup

--- john-smilga/09-file-upload/app.js
+++ john-smilga/09-file-upload/app.js
@@ -6,6 +6,15 @@ const app = express();
 
 const fileUpload = require("express-fileupload");
 
+//use v2
+const cloudinary = require("cloudinary").v2;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
+
 // database
 const connectDB = require("./db/connect");
 
--- john-smilga/09-file-upload/controllers/uploadsController.js
+++ john-smilga/09-file-upload/controllers/uploadsController.js
@@ -1,9 +1,28 @@
 const path = require("path");
 
 const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
 
 const uploadProductImage = async (req, res) => {
-  let productImage = req.files.image;
+  // check if file exists
+  // check format
+  // check size
+
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No file Uploaded");
+  }
+
+  const productImage = req.files.image;
+
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please upload Image");
+  }
+  const maxSize = 10 * 1024 * 1024;
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError(
+      "Please upload Image smaller than 1kb"
+    );
+  }
 
   const imagePath = path.join(
     __dirname,
