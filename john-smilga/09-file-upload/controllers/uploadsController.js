278f35d3e0f815b0f20ae3d4e366ca6216f2974d
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-19 19:33:08 +0530

    express file upload

--- john-smilga/09-file-upload/controllers/uploadsController.js
+++ john-smilga/09-file-upload/controllers/uploadsController.js
@@ -1,7 +1,20 @@
+const path = require("path");
+
 const { StatusCodes } = require("http-status-codes");
 
 const uploadProductImage = async (req, res) => {
-  res.send("Upload Product Image");
+  let productImage = req.files.image;
+
+  const imagePath = path.join(
+    __dirname,
+    "../public/uploads/" + `${productImage.name}`
+  );
+
+  await productImage.mv(imagePath);
+
+  return res
+    .status(StatusCodes.OK)
+    .json({ image: { src: `/uploads/${productImage.name}` } });
 };
 
 module.exports = { uploadProductImage };
