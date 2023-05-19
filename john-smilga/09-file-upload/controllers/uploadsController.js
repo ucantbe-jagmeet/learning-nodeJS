f7a6a3812856358a2e865ac5553f479b81b9aa5a
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-05-19 20:59:57 +0530

    remove temp files

--- john-smilga/09-file-upload/controllers/uploadsController.js
+++ john-smilga/09-file-upload/controllers/uploadsController.js
@@ -4,6 +4,7 @@ const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 
 const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
 
 const uploadProductImageLocal = async (req, res) => {
   // check if file exists
@@ -48,6 +49,7 @@ const uploadProductImage = async (req, res) => {
       folder: "file-upload",
     }
   );
+  fs.unlinkSync(req.files.image.tempFilePath);
   return res.status(StatusCodes.OK).json({ image: { src: result.secure_url } });
 };
 
