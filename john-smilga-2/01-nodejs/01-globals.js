f56e09759c87d57aa9b752e969c6a26b521048c2
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-12 02:18:05 +0530

    globals

---
+++ john-smilga-2/01-nodejs/01-globals.js
@@ -0,0 +1,12 @@
+// Globals - no window
+
+// __dirname - path tp current directory
+// __filename - file name
+// require - function to use modules
+// module - info about current module
+// process - info about env where the program is being executed
+
+console.log(__dirname);
+setInterval(() => {
+  console.log("Hello World");
+}, 1000);
