1ad3d22d6e5e865ee7becde2836a71baefdd8b51
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-13 03:58:21 +0530

    http module more features

--- john-smilga-2/01-nodejs/06-http-module.js
+++ john-smilga-2/01-nodejs/06-http-module.js
@@ -1,8 +1,18 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.write("Welcome to our home page");
-  res.end();
+  if (req.url === "/") {
+    return res.end("Welcome to our home page! hurray");
+  }
+  if (req.url === "/about") {
+    return res.end("Here is our short story");
+  }
+
+  return res.end(`
+    <h1>OOps!</h1>
+    <p>We cant find page</p>
+    <a href='/'>back home</a>
+    `);
 });
 
 server.listen(5000);
