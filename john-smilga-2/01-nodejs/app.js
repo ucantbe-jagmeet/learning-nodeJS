8d0782a5dfbeaa2eef2deef0fed1afe046c00a8a
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-14 18:29:57 +0530

    Event loop examples

--- john-smilga-2/01-nodejs/app.js
+++ john-smilga-2/01-nodejs/app.js
@@ -1,1 +1,28 @@
-console.log("this is jagmeet here");
+const { readFile, writeFile } = require("fs");
+
+/*
+
+
+console.log("Started a first task");
+// Check file path
+
+readFile("./content/first.txt", "utf8", (err, result) => {
+    if (err) {
+        console.log(err);
+        return;
+    }
+    
+    console.log(result);
+    console.log("compiled first task");
+});
+
+console.log("Starting next task");
+
+*/
+
+console.log("first");
+setTimeout(() => {
+  console.log("Second");
+}, 0);
+
+console.log("Third");
