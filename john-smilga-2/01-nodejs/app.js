593b7fdf4f93b222bdf5e74c84cc404a0b3e3b50
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-14 19:45:54 +0530

    Async Patterns setup promises

--- john-smilga-2/01-nodejs/app.js
+++ john-smilga-2/01-nodejs/app.js
@@ -1,21 +1,42 @@
-const http = require("http");
+/*
+    const http = require("http");
 
-const server = http.createServer((req, res) => {
-  if (req.url === "/") {
-    res.end("Home Page");
-  }
-  if (req.url === "/about") {
-    // blocking code
-    for (let i = 0; i < 1000; i++) {
-      for (let j = 0; j < 1000; j++) {
-        console.log(`${i} ${j}`);
-      }
+    const server = http.createServer((req, res) => {
+    if (req.url === "/") {
+        res.end("Home Page");
     }
-    res.end("About Page");
-  }
-  res.end("Error Page");
-});
+    if (req.url === "/about") {
+        // blocking code
+        for (let i = 0; i < 1000; i++) {
+        for (let j = 0; j < 1000; j++) {
+            console.log(`${i} ${j}`);
+        }
+        }
+        res.end("About Page");
+    }
+    res.end("Error Page");
+    });
 
-server.listen(5000, () => {
-  console.log("server is listening on port 5000 ...");
-});
+    server.listen(5000, () => {
+    console.log("server is listening on port 5000 ...");
+    });
+
+*/
+
+const { readFile } = require("fs");
+
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+getText("./content/first.txt")
+  .then((result) => console.log(result))
+  .catch((err) => console.log(err));
