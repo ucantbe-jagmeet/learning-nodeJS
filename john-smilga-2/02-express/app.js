7e0df40cd0212033c949892693acb7fe9dd8a20d
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-15 21:19:42 +0530

    http header

--- john-smilga-2/02-express/app.js
+++ john-smilga-2/02-express/app.js
@@ -1,8 +1,9 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  console.log("user hit the server");
-  res.end("Home Page");
+  res.writeHead(200, { "content-type": "text/html" });
+  res.write("<h1>home page</h1>");
+  res.end();
 });
 
 server.listen(5000);
