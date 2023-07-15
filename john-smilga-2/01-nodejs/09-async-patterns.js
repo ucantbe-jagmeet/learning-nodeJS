dd3860e85b2ab7cdae98559080bb7c3ea939281e
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-15 17:17:29 +0530

    Events emitter code example

---
+++ john-smilga-2/01-nodejs/09-async-patterns.js
@@ -0,0 +1,51 @@
+// const http = require("http");
+
+// const server = http.createServer((req, res) => {
+//   if (req.url === "/") {
+//     res.end("Home Page");
+//   }
+//   if (req.url === "/about") {
+//     // blocking code
+//     for (let i = 0; i < 1000; i++) {
+//       for (let j = 0; j < 1000; j++) {
+//         console.log(`${i} ${j}`);
+//       }
+//     }
+//     res.end("About Page");
+//   }
+//   res.end("Error Page");
+// });
+
+// server.listen(5000, () => {
+//   console.log("server is listening on port 5000 ...");
+// });
+
+const { readFile } = require("fs");
+
+const getText = (path) => {
+  return new Promise((resolve, reject) => {
+    readFile(path, "utf8", (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(data);
+      }
+    });
+  });
+};
+
+// getText("./content/first.txt")
+//   .then((result) => console.log(result))
+//   .catch((err) => console.log(err));
+
+const start = async () => {
+  try {
+    const first = await getText("./content/first.txt");
+    const second = await getText("./content/second.txt");
+    console.log(first, second);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+start();
--- john-smilga-2/01-nodejs/app.js
+++ john-smilga-2/01-nodejs/app.js
@@ -1,51 +1,9 @@
-// const http = require("http");
+const EventEmitter = require("events");
 
-// const server = http.createServer((req, res) => {
-//   if (req.url === "/") {
-//     res.end("Home Page");
-//   }
-//   if (req.url === "/about") {
-//     // blocking code
-//     for (let i = 0; i < 1000; i++) {
-//       for (let j = 0; j < 1000; j++) {
-//         console.log(`${i} ${j}`);
-//       }
-//     }
-//     res.end("About Page");
-//   }
-//   res.end("Error Page");
-// });
+const customEmitter = new EventEmitter();
 
-// server.listen(5000, () => {
-//   console.log("server is listening on port 5000 ...");
-// });
+customEmitter.on("response", () => {
+  console.log(`data recieved`);
+});
 
-const { readFile } = require("fs");
-
-const getText = (path) => {
-  return new Promise((resolve, reject) => {
-    readFile(path, "utf8", (err, data) => {
-      if (err) {
-        reject(err);
-      } else {
-        resolve(data);
-      }
-    });
-  });
-};
-
-// getText("./content/first.txt")
-//   .then((result) => console.log(result))
-//   .catch((err) => console.log(err));
-
-const start = async () => {
-  try {
-    const first = await getText("./content/first.txt");
-    const second = await getText("./content/second.txt");
-    console.log(first, second);
-  } catch (error) {
-    console.log(error);
-  }
-};
-
-start();
+customEmitter.emit("response");
