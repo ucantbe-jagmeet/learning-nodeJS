f1143d90800c84c5c4b387a7bd8415fe70f8d588
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-15 17:48:43 +0530

    Streams additional info

--- john-smilga-2/01-nodejs/app.js
+++ john-smilga-2/01-nodejs/app.js
@@ -1,9 +1,22 @@
 const { createReadStream } = require("fs");
 
-// stream usually read the large file into small chunks
+// const stream = createReadStream("./content/big.txt");
 
-const stream = createReadStream("./content/big.txt");
+// stream usually read the large file into small chunks
+// default 64kb
+// last buffer - remainder
+// highWaterMark - control size
+const stream = createReadStream("./content/big.txt", {
+  highWaterMark: 90000,
+  encoding: "utf8",
+});
+// const stream = createReadStream("./content/big.txt", { encoding: "utf8" });
 
 stream.on("data", (result) => {
   console.log(result.length);
+  console.log(result);
+});
+
+stream.on("error", (err) => {
+  console.log(err);
 });
