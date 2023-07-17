c9ed19c40d0e18c607e1d1dc6ff2ab4b59085621
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-17 20:20:10 +0530

    express basics

--- john-smilga-2/02-express/app.js
+++ john-smilga-2/02-express/app.js
@@ -17,11 +17,3 @@ app.all("*", (req, res) => {
 app.listen(5000, () => {
   console.log("server is listening on port 5000...");
 });
-
-// app.get
-// app.post
-// app.put
-// app.delete
-// app.all
-// app.use
-// app.listen
