f0dc6a20eed63d13eb92fa7e430bda45ef43338a
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-18 20:20:10 +0530

    express app example

--- john-smilga-2/02-express/app.js
+++ john-smilga-2/02-express/app.js
@@ -1,17 +1,16 @@
 const express = require("express");
 const app = express();
+const path = require("path");
+
+// setup static and middleware
+app.use(express.static("./public"));
 
 app.get("/", (req, res) => {
-  console.log("user hit the resource");
-  res.status(200).send("Home Page");
-});
-
-app.get("/about", (req, res) => {
-  res.status(200).send("About Page");
+  res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
 });
 
 app.all("*", (req, res) => {
-  res.status(404).send("<h1>resource not found</h1>");
+  res.status(404).send("Resourse not found");
 });
 
 app.listen(5000, () => {
