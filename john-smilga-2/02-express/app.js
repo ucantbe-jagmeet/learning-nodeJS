b9d78361862727af6f39538db8c1d169c438419f
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-19 12:50:25 +0530

    middleware setup

--- john-smilga-2/02-express/app.js
+++ john-smilga-2/02-express/app.js
@@ -1,58 +1,22 @@
 const express = require("express");
 const app = express();
-const { products } = require("./data");
 
-app.get("/", (req, res) => {
-  res.send("<h1> Home Page </h1> <a href='/api/products' >products</a>");
+// req => middleware => res
+
+const logger = (req, res, next) => {
+  const method = req.method;
+  const url = req.url;
+  const time = new Date().getFullYear();
+  console.log(method, url, time);
+  next();
+};
+
+app.get("/", logger, (req, res) => {
+  res.send("Home");
 });
 
-app.get("/api/products", (req, res) => {
-  const newProducts = products.map((product) => {
-    const { id, name, image } = product;
-    return { id, name, image };
-  });
-  res.json(newProducts);
-});
-
-app.get("/api/products/:productID", (req, res) => {
-  const { productID } = req.params;
-  const singleProduct = products.find(
-    (product) => product.id === Number(productID)
-  );
-
-  if (!singleProduct) {
-    return res.status(404).send("Product does not exist");
-  }
-
-  return res.json(singleProduct);
-});
-
-app.get("/api/products/:productID/reviews/:reviewID", (req, res) => {
-  console.log(req.params);
-  res.send("Hello World");
-});
-
-app.get("/api/v1/query", (req, res) => {
-  // console.log(req.query);
-  const { search, limit } = req.query;
-  let sortedProducts = [...products];
-
-  if (search) {
-    sortedProducts = sortedProducts.filter((product) => {
-      return product.name.startsWith(search);
-    });
-  }
-  if (limit) {
-    sortedProducts = sortedProducts.slice(0, Number(limit));
-  }
-
-  if (sortedProducts.length < 1) {
-    return res
-      .status(200)
-      .json({ succcess: true, msg: "No Product match your search" });
-  }
-
-  res.status(200).json(sortedProducts);
+app.get("/about", logger, (req, res) => {
+  res.send("About");
 });
 
 app.listen(5000, () => {
