4f64646ac12feb1cead6682472cebfe83a0359e4
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-19 09:20:15 +0530

    JSON basics

--- john-smilga-2/02-express/app.js
+++ john-smilga-2/02-express/app.js
@@ -1,18 +1,9 @@
 const express = require("express");
 const app = express();
-const path = require("path");
+const { products } = require("./data");
 
-// setup static and middleware
-app.use(express.static("./public"));
-
-// app.get("/", (req, res) => {
-//   res.sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
-// adding to static assets
-// SSR
-// });
-
-app.all("*", (req, res) => {
-  res.status(404).send("Resourse not found");
+app.get("/", (req, res) => {
+  res.json(products);
 });
 
 app.listen(5000, () => {
