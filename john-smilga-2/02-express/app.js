8fe72d107f9aba4c5992e5fb70a18d75dd809530
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-19 09:59:39 +0530

    params query string setup

--- john-smilga-2/02-express/app.js
+++ john-smilga-2/02-express/app.js
@@ -3,7 +3,15 @@ const app = express();
 const { products } = require("./data");
 
 app.get("/", (req, res) => {
-  res.json(products);
+  res.send("<h1> Home Page </h1> <a href='/api/products' >products</a>");
+});
+
+app.get("/api/products", (req, res) => {
+  const newProducts = products.map((product) => {
+    const { id, name, image } = product;
+    return { id, name, image };
+  });
+  res.json(newProducts);
 });
 
 app.listen(5000, () => {
