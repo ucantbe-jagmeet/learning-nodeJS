28141fdb6feff5ba01776402e4f2bdc89e87ece9
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-19 19:38:09 +0530

    methods POST javascript example

--- john-smilga-2/02-express/app.js
+++ john-smilga-2/02-express/app.js
@@ -8,8 +8,21 @@ app.use(express.static("./methods-public"));
 // parse form data
 app.use(express.urlencoded({ extended: false }));
 
+//parse hson
+app.use(express.json());
+
 app.get("/api/people", (req, res) => {
   res.status(200).json({ success: true, data: people });
+});
+
+app.post("/api/people", (req, res) => {
+  const { name } = req.body;
+  if (!name) {
+    return res
+      .status(400)
+      .json({ success: false, msg: "please provide name value" });
+  }
+  res.status(201).json({ success: true, person: name });
 });
 
 app.post("/login", (req, res) => {
