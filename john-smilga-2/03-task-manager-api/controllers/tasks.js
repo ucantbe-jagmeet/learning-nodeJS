3d8156fd27d0b7dcc38f23248b185eba486ac238
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-21 21:19:53 +0530

    All Routes

--- john-smilga-2/03-task-manager-api/controllers/tasks.js
+++ john-smilga-2/03-task-manager-api/controllers/tasks.js
@@ -1,5 +1,17 @@
 const getAllTasks = (req, res) => {
   res.send("All Items");
 };
+const createTask = (req, res) => {
+  res.send("Create Task");
+};
+const getTask = (req, res) => {
+  res.send("get single Task");
+};
+const updateTask = (req, res) => {
+  res.send("Update Task");
+};
+const deleteTask = (req, res) => {
+  res.send("deleteTask");
+};
 
-module.exports = { getAllTasks };
+module.exports = { getAllTasks, createTask, getTask, updateTask, deleteTask };
