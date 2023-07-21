d99607cff1a0f3ae427d4d61530560c180e6f94d
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-21 23:45:12 +0530

    try catch block to avoid error while sending empty req.body

--- john-smilga-2/03-task-manager-api/controllers/tasks.js
+++ john-smilga-2/03-task-manager-api/controllers/tasks.js
@@ -3,8 +3,12 @@ const getAllTasks = (req, res) => {
   res.send("All Items");
 };
 const createTask = async (req, res) => {
-  const task = await Task.create(req.body);
-  res.status(201).send({ task });
+  try {
+    const task = await Task.create(req.body);
+    res.status(201).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 const getTask = (req, res) => {
   res.send("get single Task");
