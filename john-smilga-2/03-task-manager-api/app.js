54b801f99b8086a0d7481fb574c47685f326353d
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-21 23:13:58 +0530

    dotenv

--- john-smilga-2/03-task-manager-api/app.js
+++ john-smilga-2/03-task-manager-api/app.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 const tasks = require("./routes/task");
 const connectDB = require("./db/connect");
+require("dotenv").config();
 
 //middleware
 app.use(express.json());
@@ -14,10 +15,9 @@ app.get("/hello", (req, res) => {
 app.use("/api/v1/tasks", tasks);
 
 const port = 3000;
-
 const start = async () => {
   try {
-    await connectDB();
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () => {
       console.log(`Server is listening on port ${port}...`);
     });
--- john-smilga-2/03-task-manager-api/db/connect.js
+++ john-smilga-2/03-task-manager-api/db/connect.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 
 const connectDB = (url) => {
-  return mongoose
-    .connect(url)
-    .then(() => console.log("CONNECTED TO THE DB"))
-    .catch((err) => console.log(err));
+  mongoose.connect(url, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  });
 };
 
 module.exports = connectDB;
