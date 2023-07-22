c4996720d422e6a0658e632043521c392c1daa87
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-22 23:51:23 +0530

    query params

--- john-smilga-2/04-store-api/controllers/products.js
+++ john-smilga-2/04-store-api/controllers/products.js
@@ -2,14 +2,14 @@ const Product = require("../models/product");
 
 const getAllProductsStatic = async (req, res) => {
   const products = await Product.find({
-    featured: true,
     name: "vase table",
-    nbHits: products.length,
   });
   res.status(200).json({ products });
 };
 const getAllProducts = async (req, res) => {
-  res.status(200).json({ msg: "products  route" });
+  console.log(req.query);
+  const products = await Product.find(req.query);
+  res.status(200).json({ products, nbHits: products.length });
 };
 
 module.exports = { getAllProducts, getAllProductsStatic };
