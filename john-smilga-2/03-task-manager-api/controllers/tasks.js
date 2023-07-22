6f26f8e04484468069917b1b925e7f95fdef3cad
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-22 19:47:19 +0530

    Update single task

--- john-smilga-2/03-task-manager-api/controllers/tasks.js
+++ john-smilga-2/03-task-manager-api/controllers/tasks.js
@@ -28,8 +28,21 @@ const getTask = async (req, res) => {
     res.status(500).json({ msg: error });
   }
 };
-const updateTask = (req, res) => {
-  res.send("Update Task");
+const updateTask = async (req, res) => {
+  try {
+    const { id: taskID } = req.params;
+    const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
+      new: true,
+      runValidators: true,
+    });
+
+    if (!task) {
+      return res.status(404).json({ msg: `No task with id: ${taskID}` });
+    }
+    res.status(200).json({ task });
+  } catch (error) {
+    res.status(500).json({ msg: error });
+  }
 };
 const deleteTask = async (req, res) => {
   try {
