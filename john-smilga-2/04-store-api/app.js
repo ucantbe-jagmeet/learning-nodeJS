3665a4793f5e4d5c0a1f763610f0bb0e921286a9
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-22 23:22:57 +0530

    Router

--- john-smilga-2/04-store-api/app.js
+++ john-smilga-2/04-store-api/app.js
@@ -1,11 +1,11 @@
-require("dotenv").config();
-
 const express = require("express");
 const app = express();
 
 const notFoundMiddleware = require("./middleware/not-found");
 const errorMiddleware = require("./middleware/error-handler");
 const connectDB = require("./db/connect");
+const productsRouter = require("./routes/products");
+require("dotenv").config();
 
 //middleware
 app.use(express.json());
@@ -14,6 +14,8 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send('<h1>Store API</h1> <a href="/api/v1/products">products route</a> ');
 });
+
+app.use("/api/v1/products", productsRouter);
 
 // products route
 app.use(notFoundMiddleware);
@@ -24,7 +26,7 @@ const port = 3000;
 const start = async () => {
   try {
     // connect to db
-    await connectDB(process.env.MONOG_URI);
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening on port : ${port}...`));
   } catch (error) {
     console.log(error);
