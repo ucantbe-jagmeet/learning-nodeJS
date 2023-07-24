00eb30beeea7bdcc015bef09c67fc96f2baf938f
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-25 03:40:31 +0530

    create job route

--- john-smilga-2/05-jobs-api/controllers/jobs.js
+++ john-smilga-2/05-jobs-api/controllers/jobs.js
@@ -1,15 +1,26 @@
+const Job = require("../models/Job");
+const { StatusCodes } = require("http-status-codes");
+const { BadRequestError, NotFoundError } = require("../errors");
+
 const getAllJobs = async (req, res) => {
   res.send("get all jobs");
 };
+
 const getJob = async (req, res) => {
   res.send("get job");
 };
+
 const createJob = async (req, res) => {
-  res.json(req.user);
+  req.body.createdBy = req.user.userId;
+  const job = await Job.create(req.body);
+  console.log(req.body);
+  res.status(StatusCodes.CREATED).json({ job });
 };
+
 const updateJob = async (req, res) => {
   res.send("update job");
 };
+
 const deleteJob = async (req, res) => {
   res.send("delete job");
 };
