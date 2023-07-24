f67b9a5ccc8bb346a8355100adaddab6aabb9d7c
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-25 04:06:21 +0530

    get single job

--- john-smilga-2/05-jobs-api/controllers/jobs.js
+++ john-smilga-2/05-jobs-api/controllers/jobs.js
@@ -3,12 +3,26 @@ const { StatusCodes } = require("http-status-codes");
 const { BadRequestError, NotFoundError } = require("../errors");
 
 const getAllJobs = async (req, res) => {
-  const jobs = await Job.find({});
+  const jobs = await Job.find({ createdBy: req.user.userId }).sort("cretedAt");
   res.status(StatusCodes.OK).json({ jobs });
 };
 
 const getJob = async (req, res) => {
-  res.send("get job");
+  const {
+    user: { userId },
+    params: { id: jobId },
+  } = req;
+
+  const job = await Job.findOne({
+    _id: jobId,
+    createdBy: userId,
+  });
+
+  if (!job) {
+    throw new NotFoundError(`No job with id: ${jobId}`);
+  }
+
+  res.status(StatusCodes.OK).json({ job });
 };
 
 const createJob = async (req, res) => {
