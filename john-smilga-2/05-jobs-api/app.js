72e4abe83622f1332d57d0261d0d164bb91c6939
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-25 04:55:07 +0530

    security setup

--- john-smilga-2/05-jobs-api/app.js
+++ john-smilga-2/05-jobs-api/app.js
@@ -1,5 +1,11 @@
 require("dotenv").config();
 require("express-async-errors");
+
+const helment = require("helmet");
+const cors = require("cors");
+const xss = require("xss-clean");
+const rateLimiter = require("express-rate-limit");
+
 const express = require("express");
 const app = express();
 
@@ -15,8 +21,18 @@ const notFoundMiddleware = require("./middleware/not-found");
 const errorHandlerMiddleware = require("./middleware/error-handler");
 const authenticateUser = require("./middleware/authentication");
 
+app.set("trust proxy", 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 app.use(express.json());
 // extra packages
+app.use(helmet());
+app.use(cors());
+app.use(xss());
 
 //routes
 app.use("/api/v1/auth", authRouter);
