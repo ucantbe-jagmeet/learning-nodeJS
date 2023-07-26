57b2642221372e03778c278d7937199c5d6dcc8d
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-26 22:26:21 +0530

    complete file upload

--- john-smilga-2/07-file-upload/controllers/productController.js
+++ john-smilga-2/07-file-upload/controllers/productController.js
@@ -8,7 +8,8 @@ const createProduct = async (req, res) => {
 };
 
 const getAllProducts = async (req, res) => {
-  res.send("List of products");
+  const products = await Product.find({});
+  res.status(StatusCodes.OK).json({ products });
 };
 
 module.exports = {
