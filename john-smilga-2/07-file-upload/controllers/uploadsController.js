7438bcfce6f0f981aae998f94a6e19a69a3d4354
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-26 22:28:16 +0530

    additional checks

--- john-smilga-2/07-file-upload/controllers/uploadsController.js
+++ john-smilga-2/07-file-upload/controllers/uploadsController.js
@@ -2,7 +2,17 @@ const { StatusCodes } = require("http-status-codes");
 const path = require("path");
 
 const uploadProductImage = async (req, res) => {
+  if (!req.files) {
+    throw new CustomError.BadRequestError("No File Uploaded");
+  }
   const productImage = req.files.image;
+  if (!productImage.mimetype.startsWith("image")) {
+    throw new CustomError.BadRequestError("Please Upload Image");
+  }
+  const maxSize = 1024 * 1024;
+  if (productImage.size > maxSize) {
+    throw new CustomError.BadRequestError("Please upload image smaller 1MB");
+  }
   const imagePath = path.join(
     __dirname,
     "../public/uploads/" + `${productImage.name}`
