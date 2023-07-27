91940b0a7c2d9cd9f8aeac6d5c32edc946496dd5
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-27 21:46:54 +0530

    validator package

--- john-smilga-2/09-e-commerce-api/models/User.js
+++ john-smilga-2/09-e-commerce-api/models/User.js
@@ -1,5 +1,5 @@
 const mongoose = require("mongoose");
-
+const validator = require("validator");
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -10,6 +10,11 @@ const UserSchema = new mongoose.Schema({
   email: {
     type: String,
     required: [true, "Please provide email"],
+    unique: true,
+    validate: {
+      validator: validator.isEmail,
+      message: "Please provide valid email",
+    },
   },
   password: {
     type: String,
