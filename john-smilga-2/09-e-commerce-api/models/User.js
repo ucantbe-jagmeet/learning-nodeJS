8a1843de74c029ba6c2a4a1a17c30696e6bbf39c
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-27 23:10:01 +0530

    hash password

--- john-smilga-2/09-e-commerce-api/models/User.js
+++ john-smilga-2/09-e-commerce-api/models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
+
 const UserSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -27,5 +29,15 @@ const UserSchema = new mongoose.Schema({
     default: "user",
   },
 });
+
+UserSchema.pre("save", async () => {
+  this.salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+});
+
+UserSchema.methods.comparePassword = async (candidatePassword) => {
+  const isMatch = await bcrypt.compare(candidatePassword, this.password);
+  return isMatch;
+};
 
 module.exports = mongoose.model("User", UserSchema);
