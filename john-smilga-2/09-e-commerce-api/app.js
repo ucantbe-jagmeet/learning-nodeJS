466389581d2fcfcbdf10db819a0363c1a7ff8179
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-27 21:08:53 +0530

    connection to db

--- john-smilga-2/09-e-commerce-api/app.js
+++ john-smilga-2/09-e-commerce-api/app.js
@@ -1,9 +1,13 @@
+require("dotenv").config();
+
 const express = require("express");
+const connectDB = require("./db/connect");
 const app = express();
 
 const port = process.env.PORT || 3000;
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
--- john-smilga-2/09-e-commerce-api/db/connect.js
+++ john-smilga-2/09-e-commerce-api/db/connect.js
@@ -1,4 +1,5 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
 
 const connectDB = (url) => {
   return mongoose.connect(url);
