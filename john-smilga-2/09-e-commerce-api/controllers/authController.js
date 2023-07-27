45841902f84d84384b9415bf2201e3ff200fec52
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-27 22:42:55 +0530

    register unique email

--- john-smilga-2/09-e-commerce-api/controllers/authController.js
+++ john-smilga-2/09-e-commerce-api/controllers/authController.js
@@ -3,12 +3,19 @@ const { StatusCodes } = require("http-status-codes");
 const CustomError = require("../errors");
 
 const register = async (req, res) => {
+  const { email } = req.body;
+  const emailAlreadyExists = await User.findOne({ email });
+  if (emailAlreadyExists) {
+    throw new CustomError.BadRequestError("Email already exists");
+  }
   const user = await User.create(req.body);
   res.status(StatusCodes.CREATED).json({ user });
 };
+
 const login = async (req, res) => {
   res.send("login user");
 };
+
 const logout = async (req, res) => {
   res.send("logout user");
 };
