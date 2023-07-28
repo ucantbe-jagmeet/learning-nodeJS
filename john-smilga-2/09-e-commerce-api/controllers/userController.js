71bc12482395ae727d64d26e8379c699d7cb8bdb
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-28 22:35:20 +0530

    getAllUsers and get single user

--- john-smilga-2/09-e-commerce-api/controllers/userController.js
+++ john-smilga-2/09-e-commerce-api/controllers/userController.js
@@ -1,9 +1,18 @@
+const User = require("../models/User.js");
+const { StatusCodes } = require("http-status-codes");
+const CustomError = require("../errors");
+
 const getAllUsers = async (req, res) => {
-  res.send("get all users");
+  const users = await User.find({ role: "user" }).select("-password");
+  res.status(StatusCodes.OK).json({ users });
 };
 
 const getSingleUser = async (req, res) => {
-  res.send("get Single User");
+  const user = await User.findOne({ _id: req.params.id }).select("-password");
+  if (!user) {
+    throw new CustomError.NotFoundError(`No user with id : ${req.params.id}`);
+  }
+  res.status(StatusCodes.OK).json({ user });
 };
 
 const showCurrentUser = async (req, res) => {
