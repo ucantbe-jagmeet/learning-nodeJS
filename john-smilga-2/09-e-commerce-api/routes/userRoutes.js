95a571a8f859df60dbc242c98925495f73f279dd
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-28 23:12:07 +0530

    autheniticate user

--- john-smilga-2/09-e-commerce-api/routes/userRoutes.js
+++ john-smilga-2/09-e-commerce-api/routes/userRoutes.js
@@ -7,11 +7,12 @@ const {
   updateUser,
   updateUserPassword,
 } = require("../controllers/userController");
+const { authenticateUser } = require("../middleware/authentication");
 
-router.route("/").get(getAllUsers);
+router.route("/").get(authenticateUser, getAllUsers);
 router.route("/showMe").get(showCurrentUser);
 router.route("/updateUser").patch(updateUser);
 router.route("/updateUserPassword").patch(updateUserPassword);
-router.route("/:id").get(getSingleUser);
+router.route("/:id").get(authenticateUser, getSingleUser);
 
 module.exports = router;
