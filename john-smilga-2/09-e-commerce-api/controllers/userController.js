aedbadfd9a797897828ce36d8f1965bd979a19a7
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-29 22:25:13 +0530

    update password controller

--- john-smilga-2/09-e-commerce-api/controllers/userController.js
+++ john-smilga-2/09-e-commerce-api/controllers/userController.js
@@ -24,7 +24,20 @@ const updateUser = async (req, res) => {
 };
 
 const updateUserPassword = async (req, res) => {
-  res.send("update user password");
+  const { oldPassword, newPassword } = req.body;
+  if (!oldPassword || !newPassword) {
+    throw new CustomError.BadRequestError("Please provide both values");
+  }
+
+  const user = await User.findOne({ _id: req.user.userId });
+
+  const isPasswordCorrect = await user.comparePassword(oldPassword);
+
+  if (!isPasswordCorrect) {
+    throw new CustomError.UnauthenticatedError("Invalid Credentials");
+  }
+  await user.save();
+  res.status(StatusCodes.OK).json({ msg: "Success! password updated..." });
 };
 
 module.exports = {
