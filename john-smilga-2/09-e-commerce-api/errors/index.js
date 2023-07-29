41592f728ff274a9c7ec6ec81ade98caf843855d
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-29 20:48:49 +0530

    authorize permission

--- john-smilga-2/09-e-commerce-api/errors/index.js
+++ john-smilga-2/09-e-commerce-api/errors/index.js
@@ -1,8 +1,10 @@
-const CustomAPIError = require('./custom-api');
-const UnauthenticatedError = require('./unauthenticated');
-const NotFoundError = require('./not-found');
-const BadRequestError = require('./bad-request');
+const CustomAPIError = require("./custom-api");
+const UnauthenticatedError = require("./unauthenticated");
+const NotFoundError = require("./not-found");
+const BadRequestError = require("./bad-request");
+const UnauthorizedError = require("./unautorized");
 module.exports = {
+  UnauthorizedError,
   CustomAPIError,
   UnauthenticatedError,
   NotFoundError,
--- john-smilga-2/09-e-commerce-api/routes/userRoutes.js
+++ john-smilga-2/09-e-commerce-api/routes/userRoutes.js
@@ -7,9 +7,14 @@ const {
   updateUser,
   updateUserPassword,
 } = require("../controllers/userController");
-const { authenticateUser } = require("../middleware/authentication");
+const {
+  authenticateUser,
+  authorizePermissions,
+} = require("../middleware/authentication");
 
-router.route("/").get(authenticateUser, getAllUsers);
+router
+  .route("/")
+  .get(authenticateUser, authorizePermissions("admin", "owner"), getAllUsers);
 router.route("/showMe").get(showCurrentUser);
 router.route("/updateUser").patch(updateUser);
 router.route("/updateUserPassword").patch(updateUserPassword);
