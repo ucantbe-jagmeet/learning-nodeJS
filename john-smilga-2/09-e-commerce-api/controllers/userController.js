1cdf7191c465b971be196ceb979bf3f53b76abe7
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-30 20:20:10 +0530

    update user with user.save()

--- john-smilga-2/09-e-commerce-api/controllers/userController.js
+++ john-smilga-2/09-e-commerce-api/controllers/userController.js
@@ -19,21 +19,39 @@ const showCurrentUser = async (req, res) => {
   res.status(StatusCodes.OK).json({ user: req.user });
 };
 
+// update user with findone and update
+// const updateUser = async (req, res) => {
+//   const { email, name } = req.body;
+//   if (!email || !name) {
+//     throw new CustomError.BadRequestError("Please provide all values");
+//   }
+
+//   const user = await User.findOneAndUpdate(
+//     { _id: req.user.userId },
+//     { email, name },
+//     { new: true, runValidators: true }
+//   );
+
+//   const tokenUser = createTokenUser(user);
+//   attachCookiesToResponse({ res, user: tokenUser });
+
+//   res.status(StatusCodes.OK).json({ user: tokenUser });
+// };
+
 const updateUser = async (req, res) => {
   const { email, name } = req.body;
   if (!email || !name) {
     throw new CustomError.BadRequestError("Please provide all values");
   }
+  const user = await User.findOne({ _id: req.user.userId });
 
-  const user = await User.findOneAndUpdate(
-    { _id: req.user.userId },
-    { email, name },
-    { new: true, runValidators: true }
-  );
+  user.email = email;
+  user.name = name;
+
+  await user.save();
 
   const tokenUser = createTokenUser(user);
   attachCookiesToResponse({ res, user: tokenUser });
-
   res.status(StatusCodes.OK).json({ user: tokenUser });
 };
 
