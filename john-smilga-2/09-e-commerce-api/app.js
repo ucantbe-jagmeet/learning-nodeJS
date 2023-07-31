2e9d1ac975c192adc9774206e81a7692c43fc853
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-31 10:52:44 +0530

    product routes setup

--- john-smilga-2/09-e-commerce-api/app.js
+++ john-smilga-2/09-e-commerce-api/app.js
@@ -14,6 +14,7 @@ const connectDB = require("./db/connect");
 // router
 const authRouter = require("./routes/authRoutes");
 const userRouter = require("./routes/userRoutes");
+const productRouter = require("./routes/productRoutes");
 
 //middleware
 const notFoundMiddleware = require("./middleware/not-found");
@@ -32,6 +33,7 @@ app.get("/", (req, res) => {
 
 app.use("/api/v1/auth", authRouter);
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/products", productRouter);
 
 app.use(notFoundMiddleware);
 app.use(errorHandlerMiddleware);
