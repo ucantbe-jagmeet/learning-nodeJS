16cc8698af868408ee30aa2068ee117ac1ba59fa
Jagmeet Singh <jagmeetsingh2921@gmail.com>
2023-07-31 15:13:57 +0530

    create review controller

--- john-smilga-2/09-e-commerce-api/controllers/reviewController.js
+++ john-smilga-2/09-e-commerce-api/controllers/reviewController.js
@@ -6,7 +6,28 @@ const CustomError = require("../errors");
 const { checkPermissions } = require("../utils");
 
 const createReview = async (req, res) => {
-  res.send("create review");
+  const { product: productId } = req.body;
+
+  const isValidProduct = await Product.findOne({ _id: productId });
+
+  if (!isValidProduct) {
+    throw new CustomError.NotFoundError(`No product with id : ${productId}`);
+  }
+
+  const alreadySubmitted = await Review.findOne({
+    product: productId,
+    user: req.user.userId,
+  });
+
+  if (alreadySubmitted) {
+    throw new CustomError.BadRequestError(
+      "Already submitted review for this product"
+    );
+  }
+
+  req.body.user = req.user.userId;
+  const review = await Review.create(req.body);
+  res.status(StatusCodes.CREATED).json({ review });
 };
 const getAllReviews = async (req, res) => {
   res.send("get all reviews");
